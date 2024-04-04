d1e369956dc0e027d0ae84787db30a5aeb9f063b
sestradagarcia <sestradagarcia>
2024-04-04 11:54:16 +0100

    about page ui redesign touches

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,14 +6,14 @@ import Grid from "../components/Grid";
 export default function About() {
     const navigate = useNavigate();
 
-    let tile0;
-    let tile1;
+    // let tile0;
+    // let tile1;
 
     const tiles= ['x','o','x','o','o','x','o','x','x']
 
-    onMount(() => {
-        tile0.animate({ alpha: 1, scale: 1.1 }, { duration: 500 }).start();
-      });
+    // onMount(() => {
+    //     tile0.animate({ alpha: 1, scale: 1.1 }, { duration: 500 }).start();
+    //   });
   return (
     <View onBack={()=>navigate('/menu')}>
         <Text style={{fontSize: 60}}>
@@ -22,11 +22,12 @@ export default function About() {
         <Grid x={700} y={100} color={0xffffffff}/>
         <For each={tiles}>
                 {(tile, idx) => (
-                    <Text ref={`${"tile"+idx}`} style={{ x: (idx() % 3) * 300 + 525, y: ~~(idx() / 3) * 300 + 200, color: 0x40ffffff, zIndex: 1000, alpha:0}}>
+                    <Text style={{ x: (idx() % 3) * 300 + 470, y: ~~(idx() / 3) * 300 + 200, color: 0x40ffffff, zIndex: 1000}}> {/*ref={`${"tile"+idx}`}*/}
                         {tile}
                     </Text>
                 )}
-            </For>
+        </For>
+        <View style={{ width: 5, height: 900, x: 1155, y: 100, color:0xffffffff }} />
     </View>
   )
 }
